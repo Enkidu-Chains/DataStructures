98c34816833b6bcf3fff5422fb3b14e28e2102d0
Vladyslav Pyrch <vlad.nuron@gmail.com>
2023-12-18 16:24:07 +0100

    updated functionality assuming the valid will always be passed.

--- Stack/stack.h
+++ Stack/stack.h
@@ -17,8 +17,8 @@ struct Stack {
 
 /**
  * \brief Takes in capacity and creates a new stack with that capacity.
- * \param capacity The capacity of a new stack.
- * \return A pointer to a new stack;<br>NULL pointer if capacity is less than 1.
+ * \param capacity The capacity of a new stack. Capacity should be bigger than or equal to 1.
+ * \return A pointer to a new stack;
  */
 stack_t* new_stack(int capacity);
 
@@ -27,43 +27,41 @@ stack_t* new_stack(int capacity);
  * \brief Pushes an element in the end of the stack.
  * \param stack A pointer to a stack to puch an element in.
  * \param element An elenment to puch in a stack.
- * \return 0 if an element is successfully pushed;<br>-1 if a pointer to a stack is NULL pointer or if a stack is full.
  */
-int push(stack_t* stack, int element);
+void push(stack_t* stack, int element);
 
 /**
- * \brief Removes the last element form the stack.
+ * \brief Removes the last element form the stack and returns it.
  * \param stack A pointer to a stack to pop the last element form.
- * \return 0 if the last element is successfully poped out of a stack;<br>-1 if A pointer to a stack is NULL pointer or if a stack is empty.
+ * \return a removed element.
  */
 int pop(stack_t* stack);
 
 /**
- * \brief Returns a pointer to the last element in a stack.
+ * \brief Returns the last element in a stack.
  * \param stack A pointer to stack to peek the last element form.
- * \return A pointer to the last element in a stack;<br>NULL pointer if a pointer to a stak is NULL pointer or if a stakc is empty.
+ * \return the last element.
  */
-int* peek(const stack_t* stack);
+int peek(const stack_t* stack);
 
 /**
  * \brief Checks wether a stack is empty or not.
  * \param stack A pointer to a stack to check wether it is empty.
- * \return 1 if a stack is empty;<br>0 if a stack has at least one elemnet or a pointer to it is NULL.
+ * \return 1 if a stack is empty;<br>0 if a stack has at least one elemnet.
  */
 int is_empty(const stack_t* stack);
 
 /**
  * \brief Checks wether a stack is full or not.
  * \param stack A pointer to a stack to check wether it is full.
- * \return 1 if a stack is full;<br>0 if a stack has at least one elemnet or a pointer to it is NULL.
+ * \return 1 if a stack is full;<br>0 if a stack has at least one elemnet.
  */
 int is_full(const stack_t* stack);
 
 /**
  * \brief Clears the allocated to a stack memory and sets the pointer to a stack to NULL pointer.
  * \param stack A pointer to a pointer to a stack to clear.
- * \return 0 if stack is successfully cleard;<br>-1 if parameter stack is NULL pointer or points to NULL pointer.
  */
-int clear(stack_t** stack);
+void clear(stack_t** stack);
 
 #endif //STACK_H
